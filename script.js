01854f0b9f9cc93d2935c14cf2eefae448f007d9
tarik <tarikozgur0607@gmail.com>
2022-06-04 14:15:25 +0300

    add conditions

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
 
-//Bilgisayarın seçimleri
+//computer Play
 function computerPlay(){
   let computer_choices = ["Rock","Paper","Scissors"]; 
   let random_choice =computer_choices[Math.floor(Math.random()*computer_choices.length)] 
   return random_choice;
 }
 console.log(computerPlay())
-//kimin kazandığını kontrol
-function gameControl(playerSelection,computerSelection){
-
+//Play round
+function playRound(playerSelection,computerSelection){
+  //player lose
+  if (playerSelection ==="Rock"&& computerSelection==="Scissors"  ||playerSelection ==="Paper" && computerSelection ==="Rock" || playerSelection==="Scissors"&& computerSelection==="Paper" ) {
+    return "player win";
+  }
+  //computer win
+  
+  //win
 }
-//oyunu kimin kazandığını kontrol
+//game
 function game() {
   for (let i = 0; i <5; i++) {
     
   }
 }
-//oyuncu seçimi
-const playerSelection = "Rock";
-//bilgisayar seçimi
+//player selection
+const playerSelection = prompt("Rock, Paper,Scissors?");
+//computer selection
 const computerSelection = computerPlay();
-console.log(gameControl(playerSelection,computerSelection))
+console.log(playRound(playerSelection,computerSelection))
