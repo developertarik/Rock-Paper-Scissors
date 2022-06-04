05a0c13a074c8e792ac11f92f3ac63615199c562
tarik <tarikozgur0607@gmail.com>
2022-06-04 13:24:31 +0300

    add some  functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Bilgisayarın seçimleri
+function computerPlay(){
+  computerSelection = ["Rock","Paper","Scissors"]
+}
+//kimin kazandığını kontrol
+function gameControl(playerSelection,computerSelection){
 
+}
+//oyunu kimin kazandığını kontrol
+function game() {
+  for (let i = 0; i <5; i++) {
+    
+  }
+}
+//oyuncu seçimi
+const playerSelection = "Rock";
+//bilgisayar seçimi
+const computerSelection = computerPlay();
+console.log(gameControl(playerSelection,computerSelection))
